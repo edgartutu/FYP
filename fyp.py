3146636090cbb850e96bea48faaf7f3ed39b7693
Alvin Abaho <alvinabaho@gmail.com>
2019-06-14 09:53:10 +0300

    [Ed & Barn] removed unused import statements

--- fyp.py
+++ fyp.py
@@ -1,9 +1,6 @@
 from project import app, db
-from flask import render_template, redirect, request, url_for, flash, abort
-from flask_login import login_user, login_required, logout_user
 from project.models import User, Admin, Proposal, Department
 from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
 
 api = Api(app)
 
