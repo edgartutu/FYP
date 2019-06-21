907107d99ba8b366f1dbf70f4f9ee52356c40da6
edgartutu <walugembe.john@gmail.com>
2019-06-21 20:58:06 +0300

    runs our app

---
+++ main.py
@@ -0,0 +1,37 @@
+from project import app, db
+from routes import generate_routes
+from flask_restful import Resource, Api
+from flask_restful import Api
+from project.users.views import Register,Login,Logout,ResetPassword,GetAllProjects,PostProjects,ViewPrjects
+from project.guest.views import Login,Logout,AssignedProposal
+from project.admin.views import Login,Logout,ApproveProject,PostProject,PendingProposal,ProposalComment
+
+
+
+api = Api(app)
+
+
+##api.add_resource(Register, '/register')
+##api.add_resource(Login, '/login-user')
+##api.add_resource(Logout, '/logout')
+##api.add_resource(ResetPassword, '/reset-password')
+##api.add_resource(GetAllProjects, '/getprojects')
+api.add_resource(PostProjects, '/postprojects')
+##api.add_resource(ViewPrjects, '/viewprojects')
+##
+##api.add_resource(Login, '/login-admin')
+##api.add_resource(Logout, '/logout-user')
+##api.add_resource(ApproveProject, '/approve')
+##api.add_resource(PostProject, '/postproject')
+##api.add_resource(PendingProposal, '/pendingproposal')
+##api.add_resource(ProposalComment, '/proposalcomment')
+##
+##api.add_resource(Login, '/login-guest')
+##api.add_resource(Logout, '/logout-guest')
+##api.add_resource(AssignedProposal, '/viewproposals')
+
+         
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
