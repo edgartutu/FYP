71f3e171baeeb1e8e715cdcaaa0ae62afe468ba9
edgartutu <walugembe.john@gmail.com>
2019-06-21 20:47:43 +0300

    deleteion of unused files

--- FYP/project/guest/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import Form
-from wtforms import TextField, PasswordField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
-
-class LoginForm(Form):
-    email = TextField('registration n0', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
